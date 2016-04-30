d318c354a3121d5af1e75535dabebb5021d610e1
Leich <you@example.com>
2016-04-30 19:17:42 +0000

    Commit 4

--- checkos.sh
+++ checkos.sh
@@ -3,3 +3,15 @@
 cat /proc/version
 lsb_release -a
 cat /etc/*-release | grep NAME
+
+
+condition=$(cat /etc/*release | grep "Raspbian" | wc -l)
+if [ -z $condition ] ; then
+    echo "Test"
+fi
+
+condition=$(cat /etc/*release | grep "Ubunutu" | wc -l)
+if [ -z $condition ] ; then
+    echo "Test 2"
+fi
+
