aeb987ad2fbfc7af80689ee00570a278ef31f8dd
rbeaden <rich.beaden@gmail.com>
2018-02-16 15:50:00 +0000

    Updated Systeupdate
    Installs Chome

--- systemupdate.sh
+++ systemupdate.sh
@@ -49,6 +49,19 @@ if [ $condition -eq 0 ] ; then
     echo "installation complete"
 fi
 
+program="google-chrome-stable"
+condition=$(which $program 2>/dev/null | grep -v "not found" | wc -l)
+if [ $condition -eq 0 ] ; then
+    echo "$program is not installed"
+    echo "installing $program"
+    echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | su$
+    wget https://dl.google.com/linux/linux_signing_key.pub
+    sudo apt-key add linux_signing_key.pub
+    sudo apt update
+    sudo apt install google-chrome-stable
+    echo "installation complete"
+fi
+
 git clone https://github.com/rbeaden/bashbunny-payloads
 git clone https://github.com/rbeaden/openvpn-install
 git clone https://github.com/rbeaden/katoolin
