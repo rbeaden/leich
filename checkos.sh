03a754390ffc63b35c088c9301e5c75611ad9aa1
rbeaden <rich.beaden@gmail.com>
2018-03-03 14:34:43 +0000

    Changes to be committed: 	modified:   checkos.sh

--- checkos.sh
+++ checkos.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-cat /proc/version
-lsb_release -a
-cat /etc/*-release | grep NAME
+#cat /proc/version
+#lsb_release -a
+cat /etc/*-release | grep 'DISTRIB_ID' | awk -F=  '{ print $2 }'
 
+#condition=$(cat /etc/*release | grep "Raspbian" | wc -l)
+#if [ -z $condition ] ; then
+#    echo "Test"
+#fi
 
-condition=$(cat /etc/*release | grep "Raspbian" | wc -l)
-if [ -z $condition ] ; then
-    echo "Test"
-fi
-
-condition=$(cat /etc/*release | grep "Ubunutu" | wc -l)
-if [ -z $condition ] ; then
-    echo "Test 2"
-fi
+#condition=$(cat /etc/*release | grep "Ubunutu" | wc -l)
+#if [ -z $condition ] ; then
+#    echo "Test 2"
+#fi
 
