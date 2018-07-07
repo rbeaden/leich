78895c672d228488f9a0f78a576f9d52645698f3
rbeaden <rich.beaden@gmail.com>
2018-07-07 10:25:13 +0100

    modified:   README 	modified:   checkos.sh

--- checkos.sh
+++ checkos.sh
@@ -2,13 +2,21 @@
 
 #cat /proc/version
 #lsb_release -a
-cat /etc/*-release | grep 'ID' | awk -F=  '{ print $2 }'
+#echo "OS:"
+#cat /etc/*-release | grep 'ID' | awk -F=  '{ print $2 }'| head -1
+#echo "Version:"
+#cat /etc/*-release | grep VERSION_CODENAME | cut -d "=" -f2
 
-#condition=$(cat /etc/*release | grep "Raspbian" | wc -l)
-#if [ -z $condition ] ; then
-#    echo "Test"
-#fi
+#echo "Staring test 1"
 
+#osversion=Ubuntu
+
+condition=$(cat /etc/*-release | grep 'ID' | awk -F=  '{ print $2 }'| head -1)
+echo $condition
+if [ "$condition" = "Ubuntu" ] ; then
+  codename=$(cat /etc/*-release | grep 'VERSION_CODENAME' | cut -d "=" -f2)
+  echo $codename
+fi
 #condition=$(cat /etc/*release | grep "Ubunutu" | wc -l)
 #if [ -z $condition ] ; then
 #    echo "Test 2"
