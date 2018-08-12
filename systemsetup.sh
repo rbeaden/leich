e54adf81dbafe7f7c5d770585486cd94dfccf08a
rbeaden <rich.beaden@gmail.com>
2018-08-12 21:37:46 +0100

    new file:   systemsetup.sh

---
+++ systemsetup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo 1. betaflightinstaller
+echo 2. nmapinstall.sh
+echo 3. vboxinstall 
+echo 4. chromeinstall.sh
+echo 5. opentxin 
+echo 6. vercyptinstaller
+echo 7. chromiuminstall.sh
+echo 8. python3install.sh
+echo 9. wpsinstall
+echo 10.  dirbinstall.sh
+echo 11. vagrantinstaller
+echo 12. all
+echo Select option\:
+read command
+echo $command
