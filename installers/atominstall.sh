8473d03e30e432febeb6617dbfcf7c82d681e759
rbeaden <rich.beaden@gmail.com>
2019-02-17 14:10:55 +0000

    Changes to be committed: 	new file:   django.txt 	new file:   installers/atominstall.sh 	new file:   installers/monerowalletinstall.sh

---
+++ installers/atominstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt update; sudo apt install atom
---
+++ installers/monerowalletinstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://downloads.getmonero.org/linux64
+tar jxvf linux64
+cd monero-v0.13.0.4/
+./monerod
+./monero-wallet-cli
