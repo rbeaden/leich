afd8853835d021008825992ba8ef657a7f821b3b
rbeaden <rich.beaden@gmail.com>
2019-03-10 21:11:24 +0000

    Changes to be committed: 	modified:   inputexample.py

--- python/inputexample.py
+++ python/inputexample.py
@@ -1,2 +1,10 @@
-name = input("What's your name? ")
-print("Nice to meet you " + name + "!")
+#!/usr/bin/env python3
+
+def main():
+    prompt = ">"
+    name = input(prompt)
+    print(prompt + name + "!")
+
+# Driver Code
+if __name__ == '__main__':
+    main()
